ed3107c1a945a2c302abd7d19ce4214314789c27
243107006@qq.com <baby2431>
2016-07-25 14:08:29 +0800

    remove static toast .it will leak

--- library/src/main/java/android/lemon/utils/T.java
+++ library/src/main/java/android/lemon/utils/T.java
@@ -5,116 +5,77 @@ import android.widget.Toast;
 
 /**
  * Toast统一管理
+ *
  * @author Kevin
  * @date 2014年12月7日 下午4:52:17
- * 
  */
 public class T {
-	// Toast
-	private static Toast toast;
 
-	/**
-	 * 短时间显示Toast
-	 * 
-	 * @param context
-	 * @param message
-	 */
-	public static void showShort(Context context, CharSequence message) {
-		if (null == toast) {
-			toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-			// toast.setGravity(Gravity.CENTER, 0, 0);
-		} else {
-			toast.setText(message);
-		}
-		toast.show();
-	}
+    /**
+     * 短时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showShort(Context context, CharSequence message) {
 
-	/**
-	 * 短时间显示Toast
-	 * 
-	 * @param context
-	 * @param message
-	 */
-	public static void showShort(Context context, int message) {
-		if (null == toast) {
-			toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-			// toast.setGravity(Gravity.CENTER, 0, 0);
-		} else {
-			toast.setText(message);
-		}
-		toast.show();
-	}
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
 
-	/**
-	 * 长时间显示Toast
-	 * 
-	 * @param context
-	 * @param message
-	 */
-	public static void showLong(Context context, CharSequence message) {
-		if (null == toast) {
-			toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
-			// toast.setGravity(Gravity.CENTER, 0, 0);
-		} else {
-			toast.setText(message);
-		}
-		toast.show();
-	}
+    /**
+     * 短时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showShort(Context context, int message) {
 
-	/**
-	 * 长时间显示Toast
-	 * 
-	 * @param context
-	 * @param message
-	 */
-	public static void showLong(Context context, int message) {
-		if (null == toast) {
-			toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
-			// toast.setGravity(Gravity.CENTER, 0, 0);
-		} else {
-			toast.setText(message);
-		}
-		toast.show();
-	}
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
 
-	/**
-	 * 自定义显示Toast时间
-	 * 
-	 * @param context
-	 * @param message
-	 * @param duration
-	 */
-	public static void show(Context context, CharSequence message, int duration) {
-		if (null == toast) {
-			toast = Toast.makeText(context, message, duration);
-			// toast.setGravity(Gravity.CENTER, 0, 0);
-		} else {
-			toast.setText(message);
-		}
-		toast.show();
-	}
+    /**
+     * 长时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showLong(Context context, CharSequence message) {
 
-	/**
-	 * 自定义显示Toast时间
-	 * 
-	 * @param context
-	 * @param message
-	 * @param duration
-	 */
-	public static void show(Context context, int message, int duration) {
-		if (null == toast) {
-			toast = Toast.makeText(context, message, duration);
-			// toast.setGravity(Gravity.CENTER, 0, 0);
-		} else {
-			toast.setText(message);
-		}
-		toast.show();
-	}
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
 
-	/** Hide the toast, if any. */
-	public static void hideToast() {
-		if (null != toast) {
-			toast.cancel();
-		}
-	}
+    /**
+     * 长时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showLong(Context context, int message) {
+
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * 自定义显示Toast时间
+     *
+     * @param context
+     * @param message
+     * @param duration
+     */
+    public static void show(Context context, CharSequence message, int duration) {
+
+        Toast.makeText(context, message, duration).show();
+    }
+
+    /**
+     * 自定义显示Toast时间
+     *
+     * @param context
+     * @param message
+     * @param duration
+     */
+    public static void show(Context context, int message, int duration) {
+
+        Toast.makeText(context, message, duration).show();
+    }
 }
