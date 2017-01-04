a636d315e0c64e2b4045a33bf290c01c13f264bc
243107006 <641114>
2017-01-04 19:01:12 +0800

    add ziputlis

--- library/src/main/java/android/lemon/utils/ByteUtils.java
+++ library/src/main/java/android/lemon/utils/ByteUtils.java
@@ -1,58 +1,127 @@
 package android.lemon.utils;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-/**
- * @author Kevin
- */
 public class ByteUtils {
-    /**
-     * byte[] 转为 对象
-     *
-     * @param bytes
-     * @return
-     */
-    public static Object byteToObject(byte[] bytes) throws Exception {
-        ObjectInputStream ois = null;
-        try {
-            ois = new ObjectInputStream(new ByteArrayInputStream(bytes));
-            return ois.readObject();
-        } finally {
-            if (ois != null) ois.close();
-        }
-    }
+	/**
+	 * ��int��ֵת��Ϊռ�ĸ��ֽڵ�byte���飬������������(��λ��ǰ����λ�ں�)��˳�� ��bytesToInt��������ʹ��
+	 * 
+	 * @param value
+	 *            Ҫת����intֵ
+	 * @return byte����
+	 */
+	public static byte[] intToBytes(int value) {
+		byte[] src = new byte[4];
+		src[3] = (byte) ((value >> 24) & 0xFF);
+		src[2] = (byte) ((value >> 16) & 0xFF);
+		src[1] = (byte) ((value >> 8) & 0xFF);
+		src[0] = (byte) (value & 0xFF);
+		return src;
+	}
 
-    /**
-     * 对象 转为 byte[]
-     *
-     * @param obj
-     * @return
-     */
-    public static byte[] objectToByte(Object obj) throws Exception {
-        ObjectOutputStream oos = null;
-        try {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            oos = new ObjectOutputStream(bos);
-            oos.writeObject(obj);
-            return bos.toByteArray();
-        } finally {
-            if (oos != null) oos.close();
-        }
-    }
+	/**
+	 * byte[] תΪ ����
+	 *
+	 * @param bytes
+	 * @return
+	 */
+	public static Object byteToObject(byte[] bytes) throws Exception {
+		ObjectInputStream ois = null;
+		try {
+			ois = new ObjectInputStream(new ByteArrayInputStream(bytes));
+			return ois.readObject();
+		} finally {
+			if (ois != null)
+				ois.close();
+		}
+	}
 
-    public static void byteToBit(byte[] bytes, StringBuilder sb) {
-        for (int i = 0; i < Byte.SIZE * bytes.length; i++)
-            sb.append((bytes[i / Byte.SIZE] << i % Byte.SIZE & 0x80) == 0 ? '0' : '1');
-    }
+	/**
+	 * ���� תΪ byte[]
+	 *
+	 * @param obj
+	 * @return
+	 */
+	public static byte[] objectToByte(Object obj) throws Exception {
+		ObjectOutputStream oos = null;
+		try {
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			oos = new ObjectOutputStream(bos);
+			oos.writeObject(obj);
+			return bos.toByteArray();
+		} finally {
+			if (oos != null)
+				oos.close();
+		}
+	}
 
-    public static String byteToBit(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < Byte.SIZE * bytes.length; i++)
-            sb.append((bytes[i / Byte.SIZE] << i % Byte.SIZE & 0x80) == 0 ? '0' : '1');
-        return sb.toString();
-    }
+	public static void byteToBit(byte[] bytes, StringBuilder sb) {
+		for (int i = 0; i < Byte.SIZE * bytes.length; i++)
+			sb.append((bytes[i / Byte.SIZE] << i % Byte.SIZE & 0x80) == 0 ? '0' : '1');
+	}
 
+	public static String byteToBit(byte[] bytes) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < Byte.SIZE * bytes.length; i++)
+			sb.append((bytes[i / Byte.SIZE] << i % Byte.SIZE & 0x80) == 0 ? '0' : '1');
+		return sb.toString();
+	}
+
+	/**
+	 * ��int��ֵת��Ϊռ�ĸ��ֽڵ�byte���飬������������(��λ��ǰ����λ�ں�)��˳�� ��bytesToInt2��������ʹ��
+	 */
+	public static byte[] intToBytes2(int value) {
+		byte[] src = new byte[4];
+		src[0] = (byte) ((value >> 24) & 0xFF);
+		src[1] = (byte) ((value >> 16) & 0xFF);
+		src[2] = (byte) ((value >> 8) & 0xFF);
+		src[3] = (byte) (value & 0xFF);
+		return src;
+	}
+
+	/**
+	 * byte������ȡint��ֵ��������������(��λ��ǰ����λ�ں�)��˳�򣬺ͺ�intToBytes��������ʹ��
+	 * 
+	 * @param src
+	 *            byte����
+	 * @param offset
+	 *            ������ĵ�offsetλ��ʼ
+	 * @return int��ֵ
+	 */
+	public static int bytesToInt(byte[] src, int offset) {
+		int value;
+		value = (int) ((src[offset] & 0xFF) | ((src[offset + 1] & 0xFF) << 8) | ((src[offset + 2] & 0xFF) << 16)
+				| ((src[offset + 3] & 0xFF) << 24));
+		return value;
+	}
+
+	/**
+	 * byte������ȡint��ֵ��������������(��λ�ں󣬸�λ��ǰ)��˳�򡣺�intToBytes2��������ʹ��
+	 */
+	public static int bytesToInt2(byte[] src, int offset) {
+		int value;
+		value = (int) (((src[offset] & 0xFF) << 24) | ((src[offset + 1] & 0xFF) << 16) | ((src[offset + 2] & 0xFF) << 8)
+				| (src[offset + 3] & 0xFF));
+		return value;
+	}
+	
+	 public static String bytesToHexString(byte[] src){
+	        StringBuilder stringBuilder = new StringBuilder("");
+	        if (src == null || src.length <= 0) {
+	            return null;
+	        }
+	        for (int i = 0; i < src.length; i++) {
+	            int v = src[i] & 0xFF;
+	            String hv = Integer.toHexString(v);
+	            stringBuilder.append("0x");
+	            if (hv.length() < 2) {
+	                stringBuilder.append(0);
+	            }
+	            stringBuilder.append(hv);
+	            stringBuilder.append(" ");
+	        }
+	        return stringBuilder.toString();
+	    }
 }
